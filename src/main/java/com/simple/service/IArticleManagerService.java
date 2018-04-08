069a3e838996662b6340cb0e53169ca6cfa274c6
S I M P L E <crescentcxm@gmail.com>
2018-04-08 20:03:17 +0800

    1,文章后台管理 2,修改favicon.ico

---
+++ src/main/java/com/simple/service/IArticleManagerService.java
@@ -0,0 +1,7 @@
+package com.simple.service;
+
+/**
+ * Create by S I M P L E on 2018/04/08 19:46:24
+ */
+public interface IArticleManagerService {
+}
---
+++ src/main/java/com/simple/service/impl/ArticleManagerImpl.java
@@ -0,0 +1,11 @@
+package com.simple.service.impl;
+
+import com.simple.service.IArticleManagerService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Create by S I M P L E on 2018/04/08 19:46:40
+ */
+@Service("iArticleManagerService")
+public class ArticleManagerImpl implements IArticleManagerService {
+}
