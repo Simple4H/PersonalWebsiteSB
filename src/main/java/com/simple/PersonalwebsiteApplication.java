4db0f012a3a06e929d683e03c42449b2931c3c4f
S I M P L E <crescentcxm@gmail.com>
2018-04-14 23:21:28 +0800

    部署上线 开发环境分离 添加404页面

--- src/main/java/com/simple/PersonalwebsiteApplication.java
+++ src/main/java/com/simple/PersonalwebsiteApplication.java
@@ -4,12 +4,18 @@ package com.simple;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
 @MapperScan("com.simple.dao")
-public class PersonalwebsiteApplication {
+public class PersonalwebsiteApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PersonalwebsiteApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PersonalwebsiteApplication.class, args);
