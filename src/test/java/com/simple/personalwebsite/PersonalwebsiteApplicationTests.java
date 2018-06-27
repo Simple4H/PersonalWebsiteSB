2e48fe92b3d1ebb6e7a292849e6ff0cd666ebfa3
S I M P L E <crescentcxm@gmail.com>
2018-06-27 21:44:31 +0800

    maven package 忽略Test类

--- src/test/java/com/simple/personalwebsite/PersonalwebsiteApplicationTests.java
+++ src/test/java/com/simple/personalwebsite/PersonalwebsiteApplicationTests.java
@@ -1,10 +1,11 @@
 package com.simple.personalwebsite;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
+@Ignore
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Slf4j
