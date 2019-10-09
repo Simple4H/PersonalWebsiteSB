f3c6a4753be756364abb0101aef4f4fad76f0ecb
Simple4H <crescentcxm@gmail.com>
2019-10-09 19:31:26 +0800

    update domain

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $.ajax({
         type        : 'post',
-        url         : 'http://localhost:5555/user/get_user_info.do',
+        url         : 'https://www.cxm520hyq.com/user/get_user_info.do',
         contentType : 'application/json;charset=utf-8',
         dataType    : 'json',
         data        :  data
